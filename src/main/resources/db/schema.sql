4ea275ae251a4b908d6156aaaf57bb111fd0e8ca
Dion <ksundong@gmail.com>
2022-04-24 00:00:34 +0900

    feat: db schema 추가

---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `uint`.`uint_test`
+(
+    id  BIGINT       NOT NULL COMMENT 'id',
+    mau INT UNSIGNED NULL COMMENT 'mau는 21억을 넘을 수 있을까',
+    CONSTRAINT uint_test_pk
+        PRIMARY KEY (id)
+)
+    COMMENT 'unsigned 타입이 존재하는 테이블';
